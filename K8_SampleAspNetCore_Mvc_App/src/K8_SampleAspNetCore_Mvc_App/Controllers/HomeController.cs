8510f2282a1dc36e5a16882bb3bdae4dc6df06c6
miteshpatel3 <miteshpatel.net@gmail.com>
2022-09-16 12:25:02 +0530

    Add project files.

---
+++ K8_SampleAspNetCore_Mvc_App/src/K8_SampleAspNetCore_Mvc_App/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using K8_SampleAspNetCore_Mvc_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace K8_SampleAspNetCore_Mvc_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ K8_SampleAspNetCore_Mvc_App/src/K8_SampleAspNetCore_Mvc_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace K8_SampleAspNetCore_Mvc_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
